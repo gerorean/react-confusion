a1ef21dd80bcaac0b95b790611f03bbca61a370a
German Rodrigo <gerorean@gmail.com>
2021-02-14 21:00:02 -0500

    configurando bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
 
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+import './App.css';
+import { Component } from 'react';
+
+
+//Coursera ES6 class.. la aplicación de clase extiende el componente
+//Subclases:
+class App extends Component {//Component es una clase de componente
+  render(){//Metodo de renderizado
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+/*
+class App extends Component {//Component es una clase de componente
+  render(){//Metodo de renderizado
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+*/
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +69,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
